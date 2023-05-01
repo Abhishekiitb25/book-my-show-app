fd47b5fa91dbc03725758e7c60f3412eef513e01
Abhishekiitb25 <109478490+Abhishekiitb25@users.noreply.github.com>
2023-05-01 11:24:06 +0000

    Day-02-NavBar

---
+++ src/pages/Home.page.jsx
@@ -0,0 +1,67 @@
+// rafce (shortcut)
+import React, { useState } from "react";
+
+// Layout HOC
+import DefaultLayoutHoc from "../layout/Default.layout";
+
+
+// Components
+import HeroCarosuel from "../components/HeroCarosuel/HeroCarosuel.Component";
+import PosterSlider from "../components/PosterSlider/PosterSlider.Component";
+import EntertainmentCard from "../components/Entertainment/EntertainmentCard.Component";
+
+const HomePage = () => {
+    const[recommendedMovies, setRecommendedMovies]=useState([]);
+    const [premierMovies, setPremierMovies]=useState([]);
+    const[onlineStreamEvents, setOnlineStreamEvents]=useState([]);
+
+    return (
+      <>
+        <HeroCarosuel />
+        <div className="container mx-auto px-4 md:px-12 my-8">
+          <h1 className="text-2xl font-bold text-gray-800 sm:ml-3 ml-0 my-3">
+            The Best of Entertainment
+          </h1>
+          <EntertainmentCard />
+        </div>
+
+        <div className="container mx-auto px-4 md:px-12 my-8">
+          <PosterSlider
+            title="Recommended Movies"
+            subject="List of recommended movies"
+            posters={recommendedMovies}
+            isDark={false}
+          />
+        </div>
+
+        <div className="bg-premier-800 py-12">
+          <div className="container mx-auto px-4 md:px-12 my-8 flex flex-col gap-3">
+            <div className="hidden md:flex">
+              <img
+                src=""
+                alt="Rupay"
+                className="w-full h-full"
+              />
+            </div>
+            <PosterSlider
+              title="Premiers"
+              subject="Brand new releases every Friday"
+              posters={premierMovies}
+              isDark={true}
+            />
+          </div>
+        </div>
+        <div className="container mx-auto px-4 md:px-12 my-8 ">
+          <PosterSlider
+            title="Online Streaming Events"
+            subject="Online Streaming Events"
+            posters={onlineStreamEvents}
+            isDark={false}
+          />
+        </div>
+      </>
+    );
+    
+}
+
+export default DefaultLayoutHoc(HomePage);
